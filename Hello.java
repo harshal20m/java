258d8f6e579c183ae4a3f015a3a9f8269ba0ee95
harshal mali <20harshalmali@gmail.com>
2025-01-02 19:01:19 +0530

    new

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello {
+    public static void main(String[] args) {
+        float i1 = Float.parseFloat(args[0]);
+        int i2 = Integer.parseInt(args[1]);
+        System.out.println(i1 + i2);
+        // for (int i = 0; i < args.length; i++) {
+
+        // System.out.println(args[i]);
+        // }
+    }
+}
---
+++ arrays.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class arrays {
+    public static void main(String[] args) {
+        // int[] numbers = { 1, 2, 5, 8 };
+        // int numbers2[] = { 2, 8, 1, 5 };
+
+        // boolean equaity = Arrays.equals(numbers, numbers2);
+
+        // Arrays.sort(numbers); // {1, 2, 5, 8}
+        // System.out.print(Arrays.toString(numbers));
+
+        // int index = Arrays.binarySearch(numbers, 5);
+        // System.out.println(equaity);
+
+        // fill array
+
+        // int array1[] = new int[5];
+        // Arrays.fill(array1, 40);
+        // System.out.println(Arrays.toString(array1));
+
+        // string operations
+        String str1 = "Hello";
+        String str2 = "hello";
+        System.out.println(str1.equals(str2)); // false
+        System.out.println(str1.equalsIgnoreCase(str2)); // true
+
+    }
+}
